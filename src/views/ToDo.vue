2e775f13f4dee96208c926201a96b2839d40fb82
MinaMaher0 <minamaher30697@gmail.com>
2020-05-12 10:07:34 +0200

    version 3

--- src/views/ToDo.vue
+++ src/views/ToDo.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="ToDo">
     <div class="title is-4 has-text-centered">
-      TODOs version 2
+      TODOs version 3
     </div>
     <div class="ToDo__addNew">
       <input
